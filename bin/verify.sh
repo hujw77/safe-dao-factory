5be3fb4ad46059661ddbb7bd514e926cdd8a1974
echo <hujw77@gmail.com>
2023-12-11 16:19:12 +0800

    support ethereum

--- bin/verify.sh
+++ bin/verify.sh
@@ -6,8 +6,8 @@ c3=$PWD/script/c3.json
 
 deployer=$(jq -r ".DEPLOYER" $c3)
 dao_factory=$(jq -r ".FACTORY_ADDR" $c3)
-safe_factory=0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC
-safe_singleton=0x69f4D1788e39c87893C980c06EdF4b7f686e2938
+safe_factory=0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2
+safe_singleton=0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552
 
 verify() {
   local addr; addr=$1
@@ -27,4 +27,4 @@ verify() {
     $path > script/output/$chain_id/$name.v.json)
 }
 
-verify $dao_factory 46 $(cast abi-encode "constructor(address,address)" $safe_factory $safe_singleton) src/SafeDaoFactory.sol:SafeDaoFactory
+verify $dao_factory 1 $(cast abi-encode "constructor(address,address)" $safe_factory $safe_singleton) src/SafeDaoFactory.sol:SafeDaoFactory
